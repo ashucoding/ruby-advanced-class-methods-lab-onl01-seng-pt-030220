54c630262e196f974028e0098acbd2ea5cc02c74
Shana Wiltshire <shanawiltshire@gmail.com>
2020-04-16 23:39:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,65 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+    s = self.new
+    s.save
+    s
+  end
+
+def self.new_by_name(song_name)
+  s = self.new 
+    s.name = song_name
+    s.save 
+    s
+end   
+
+def self.create_by_name (song_name)
+  s = self.new
+    s.name = song_name
+    s.save
+    s
+  end
+  
+  def self.find_by_name (song_name)
+   self.all.detect {|i| i.name == song_name}
+  end
+  
+  def self.find_or_create_by_name(play_this_song)
+    alexa_find_it = self.all.detect {|x| x.name == play_this_song}
+    if alexa_find_it == nil
+      s = self.new 
+      s.name = play_this_song
+      s.save 
+      s
+    else
+      alexa_find_it
+    end
+  end
+    
+  def self.alphabetical
+    self.all.sort_by { |x| x.name}
+  end 
+
+def self.new_from_filename(mp3_formatted_file)
+    c = self.new 
+    c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    c
+  end 
+  
+   def self.create_from_filename(mp3_formatted_file)
+    c = self.new 
+    c.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    c.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    c.save
+    c
+end 
+
+  def self.destroy_all
+    self.all.clear
+end
 
   def self.all
     @@all
@@ -9,5 +68,4 @@ class Song
   def save
     self.class.all << self
   end
-
 end
